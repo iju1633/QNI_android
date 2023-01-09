8abecf0c1c306c7fff4be3ff42130d0ee95c401e
Garfield <iju1633@gmail.com>
2023-01-09 14:05:52 +0900

    png 파일 반환을 위한 coil 라이브러리로 ImageView에 사진 띄우는 거 성공

--- app/src/main/java/com/example/imjaewook_qni/MainActivity.kt
+++ app/src/main/java/com/example/imjaewook_qni/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.imjaewook_qni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import coil.load
 
 class MainActivity : AppCompatActivity() {
+
+    // https://quickchart.io/wordcloud?format=png&text=Four score and seven years ago...
+
+    lateinit var image : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image = findViewById(R.id.image_view)
+
+        image.load("https://quickchart.io/wordcloud?format=png&text=Four score and seven years ago...")
     }
 }
