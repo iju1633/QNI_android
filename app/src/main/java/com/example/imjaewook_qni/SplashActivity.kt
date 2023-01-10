9a29320527b71e88fb17249dc99cee0f5df70fd1
Garfield <iju1633@gmail.com>
2023-01-10 22:44:59 +0900

    - 기획한 메인 테마 컬러 적용 - no navigation bar - splash 화면 구현 및 3초간 적용

---
+++ app/src/main/java/com/example/imjaewook_qni/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.imjaewook_qni
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },DURATION)
+
+    }
+    companion object {
+        private const val DURATION : Long = 3000
+    }
+}
